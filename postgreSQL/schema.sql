5503587476381691461cd9de7868fab1a126e42a
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-07-07 20:29:15 +0200

    added course and lecture models

--- postgreSQL/schema.sql
+++ postgreSQL/schema.sql
@@ -9,3 +9,19 @@ CREATE TABLE "public"."Book" (
   "authorId" INTEGER NOT NULL,
   FOREIGN KEY ("authorId") REFERENCES "public"."Author"(id)
 );
+
+CREATE TABLE "public"."Course" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  code VARCHAR(16) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "public"."Lecture" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  time TIMESTAMP NOT NULL,
+  duration INTEGER NOT NULL,
+  teacher VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  "courseId" INTEGER NOT NULL,
+  FOREIGN KEY ("courseId") REFERENCES "public"."Course"(id)
+);
