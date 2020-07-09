88f5f3f450a6d224ee442c20b1cb6dd09ccd366e
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-07-09 15:09:48 +0200

    added new schema

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ docker-compose up -d
 sleep 2
 
 # create database tables from schema file
-psql -h localhost -d bookstore -U user -f postgreSQL/schema.sql
+psql -h localhost -d bookstore -U user -f postgreSQL/schema2.sql
 
 # reads the database tabels and translate from SQL to Prisma data model
 npx prisma introspect
@@ -16,7 +16,7 @@ npx prisma introspect
 npx prisma generate
 
 # imports courses
-node src/database/addCourseFromFile.js 
+DEBUG=server* node src/database/addCourseFromFile.js 
 
 # import lectures
-DEBUG=server:webscraper:forelesning node src/webscraper/forelesninger.js 
+DEBUG=server* node src/webscraper/forelesninger.js 
--- postgreSQL/schema2.sql
+++ postgreSQL/schema2.sql
@@ -1,40 +1,66 @@
-CREATE TABLE "public"."Course" (
-"code" text  NOT NULL ,"id" SERIAL,"name" text  NOT NULL ,
-    PRIMARY KEY ("id"))
+CREATE TABLE "public"."Course"
+(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "code" text NOT NULL,
+    "name" text NOT NULL
+);
 
-CREATE TABLE "public"."Semester" (
-"id" SERIAL,"name" text  NOT NULL ,
-    PRIMARY KEY ("id"))
+CREATE TABLE "public"."Semester"
+(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" text NOT NULL
+);
 
-CREATE TABLE "public"."Teacher" (
-"id" SERIAL,"name" text  NOT NULL ,
-    PRIMARY KEY ("id"))
+CREATE TABLE "public"."Teacher"
+(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" text NOT NULL
+);
 
-CREATE TABLE "public"."Lecture" (
-"audiolink" text  NOT NULL ,"cameralink" text  NOT NULL ,"combinedlink" text  NOT NULL ,"courseid" integer  NOT NULL ,"duration" integer  NOT NULL ,"id" SERIAL,"screenlink" text  NOT NULL ,"semesterid" integer  NOT NULL ,"timeofrecording" timestamp(3)  NOT NULL ,"title" text  NOT NULL ,
-    PRIMARY KEY ("id"))
+CREATE TABLE "public"."Lecture"
+(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "audiolink" VARCHAR(255) NOT NULL ,
+    "cameralink" VARCHAR(255) NOT NULL ,
+    "combinedlink" VARCHAR(255) NOT NULL ,
+    "courseid" integer NOT NULL ,
+    "duration" integer NOT NULL ,
+    "screenlink" VARCHAR(255) NOT NULL ,
+    "semesterid" integer NOT NULL ,
+    "timeofrecording" TIMESTAMP NOT NULL ,
+    "title" VARCHAR(255) NOT NULL,
+    FOREIGN KEY ("courseid") REFERENCES "public"."Course"("id") ON DELETE CASCADE  ON UPDATE CASCADE,
+    FOREIGN KEY ("semesterid")REFERENCES "public"."Semester"("id") ON DELETE CASCADE  ON UPDATE CASCADE
+);
 
-CREATE TABLE "public"."UncategorizedLecture" (
-"audiolink" text  NOT NULL ,"cameralink" text  NOT NULL ,"combinedlink" text  NOT NULL ,"course" text  NOT NULL ,"duration" text  NOT NULL ,"id" SERIAL,"screenlink" text  NOT NULL ,"semesterid" integer  NOT NULL ,"teachers" text  NOT NULL ,"timeofrecording" timestamp(3)  NOT NULL ,"title" text  NOT NULL ,
-    PRIMARY KEY ("id"))
+CREATE TABLE "public"."UncategorizedLecture"
+(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "audiolink" text NOT NULL ,
+    "cameralink" text NOT NULL ,
+    "combinedlink" text NOT NULL ,
+    "course" text NOT NULL ,
+    "duration" text NOT NULL ,
+    "screenlink" text NOT NULL ,
+    "semesterid" integer NOT NULL ,
+    "teachers" text NOT NULL ,
+    "timeofrecording" timestamp(3) NOT NULL ,
+    "title" text NOT NULL,
+    FOREIGN KEY ("semesterid")REFERENCES "public"."Semester"("id") ON DELETE CASCADE  ON UPDATE CASCADE
+);
 
-CREATE TABLE "public"."_LectureToTeacher" (
-"A" integer  NOT NULL ,"B" integer  NOT NULL )
+CREATE TABLE "public"."_LectureToTeacher"
+(
+    "A" integer NOT NULL ,
+    "B" integer NOT NULL,
+    FOREIGN KEY ("A")REFERENCES "public"."Lecture"("id") ON DELETE CASCADE  ON UPDATE CASCADE,
+    FOREIGN KEY ("B")REFERENCES "public"."Teacher"("id") ON DELETE CASCADE  ON UPDATE CASCADE
+);
 
-CREATE UNIQUE INDEX "Semester.name" ON "public"."Semester"("name")
+CREATE UNIQUE INDEX "Semester.name" ON "public"."Semester"("name");
 
-CREATE UNIQUE INDEX "Teacher.name" ON "public"."Teacher"("name")
+CREATE UNIQUE INDEX "Teacher.name" ON "public"."Teacher"("name");
 
-CREATE UNIQUE INDEX "_LectureToTeacher_AB_unique" ON "public"."_LectureToTeacher"("A","B")
+CREATE UNIQUE INDEX "_LectureToTeacher_AB_unique" ON "public"."_LectureToTeacher"("A","B");
 
-CREATE  INDEX "_LectureToTeacher_B_index" ON "public"."_LectureToTeacher"("B")
-
-ALTER TABLE "public"."Lecture" ADD FOREIGN KEY ("courseid")REFERENCES "public"."Course"("id") ON DELETE CASCADE  ON UPDATE CASCADE
-
-ALTER TABLE "public"."Lecture" ADD FOREIGN KEY ("semesterid")REFERENCES "public"."Semester"("id") ON DELETE CASCADE  ON UPDATE CASCADE
-
-ALTER TABLE "public"."UncategorizedLecture" ADD FOREIGN KEY ("semesterid")REFERENCES "public"."Semester"("id") ON DELETE CASCADE  ON UPDATE CASCADE
-
-ALTER TABLE "public"."_LectureToTeacher" ADD FOREIGN KEY ("A")REFERENCES "public"."Lecture"("id") ON DELETE CASCADE  ON UPDATE CASCADE
-
-ALTER TABLE "public"."_LectureToTeacher" ADD FOREIGN KEY ("B")REFERENCES "public"."Teacher"("id") ON DELETE CASCADE  ON UPDATE CASCADE
+CREATE  INDEX "_LectureToTeacher_B_index" ON "public"."_LectureToTeacher"("B");
