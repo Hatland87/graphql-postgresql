3c462be20cd17144ffe6bc82acb639b592826c5b
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-07-09 14:12:18 +0200

    get semester all and semester by id, course dont work

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,31 @@
-async function courses(parent, args, context, info) {
-    return await context.prisma.course.findMany()
-}
-
-async function lectures(parent, args, context, info) {
-    return await context.prisma.lecture.findMany()
+async function semester(parent, args, context, info) {
+    return await context.prisma.semester.findMany({ where: { name: { contains: args.name} }})
 }
 
 async function semesters(parent, args, context, info) {
     return await context.prisma.semester.findMany()
+}
+
+async function course(parent, args, context, info) {
+    return await context.prisma.course.findMany({ 
+        where: { 
+            code: {
+                startsWith: args.code,
+            }
+        }
+    })
+}
+
+async function courses(parent, args, context, info) {
+    return await context.prisma.course.findMany()
+}
+
+async function teacher(parent, args, context, info) {
+    return await context.prisma.teacher.findMany({ 
+        where: { 
+            name: { startsWith: args.name } 
+        }
+    })
 }
 
 async function teachers(parent, args, context, info) {
@@ -15,8 +33,10 @@ async function teachers(parent, args, context, info) {
 }
 
 module.exports = {
-    courses,
-    lectures,
+    semester,
     semesters,
+    course,
+    courses,
+    teacher,
     teachers
 }
